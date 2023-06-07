1eee784e6075eeb645a288b7755f81c9b6c312d1
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-06-06 20:02:00 -0400

    Create a custom cell for row of tableView

---
+++ A2_WebServices/DogBreedTableView_CustomRowCell.swift
@@ -0,0 +1,23 @@
+//
+//  DogBreedTableView_CustomRowCell.swift
+//  A2_WebServices
+//
+//  Created by Bikash Chhantyal on 2023-06-06.
+//
+
+import UIKit
+
+class DogBreedTableView_CustomRowCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
