543925c758270a9d85417b8475ea4a16fb7f4a16
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-06-07 21:08:37 -0400

    Create a View Controller to display dog image and generate random image

---
+++ A2_WebServices/DogBreed_ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DogBreed_ViewController.swift
+//  A2_WebServices
+//
+//  Created by Bikash Chhantyal on 2023-06-07.
+//
+
+import UIKit
+
+class DogBreed_ViewController: UIViewController {
+
+    @IBOutlet weak var dogImage: UIImageView!
+    
+  
+    @IBAction func getRandomImage(_ sender: Any) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
